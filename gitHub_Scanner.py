2541c823865854d5bf81e191b9b506a165095cd8
LeandroDem21 <20243rd051@utez.edu.mx>
2025-09-17 16:01:27 -0600

    Agregue un bloque que busca los respositorios

--- gitHub_Scanner.py
+++ gitHub_Scanner.py
@@ -5,3 +5,16 @@ import webbrowser
 import requests
 import winreg
 
+git_dir = r "C:\Users\CC6\Desktop\docs"
+if not os.path.exists(git_dir):
+    os.makedirs(git_dir)
+    print(f"Created directory: {git_dir}")
+
+repos = [name for name in os.listdir(git.dir)
+         if os.path.isdir(os.path.join(git_dir, name)) and
+         os.path.exists(os.path.join(git_dir, name, ".git"))]
+
+if respos:
+    print("found the following Github respository")
+    for idx, repo in enumerate(repos, 1):
+        print(f"{idx . {repo}}")
